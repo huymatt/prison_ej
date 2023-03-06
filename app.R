313e8494a5d0d57aea5e42793c11736ca28e534b
huymatt <matthieu.p.huy@gmail.com>
2023-03-06 13:34:19 -0800

    fixed widgets to select state/county on map

--- app.R
+++ app.R
@@ -27,24 +27,6 @@ library(rcartocolor)
 
 
 
-### Style ###############################################
-
-bgcolor <- "#262626"
-  bg2 <- "#222223"
-
-  label_style <- list(
-    "color"        = "black",
-    "font-family"  = "default",
-    "font-weight"  = "bold",
-    "box-shadow"   = "3px 3px rgba(0,0,0,0.25)",
-    "font-size"    = "15px",
-    "border-color" = "rgba(0,0,0,0.5)")
-
-  # all colors (palette)
-  colors <- carto_pal(12, "Vivid")[c(3:5, 9)]
-
-
-
 ### Read in data ##########################################
 
 state_sf <- read_sf(here("data/tl_2022_us_state/tl_2022_us_state.shp")) |>
@@ -73,18 +55,32 @@ superfund_buffers_3m_sf <- read_sf(here("data/superfund_data/active_sites_3mile.
 prison_boundaries_sf <- read_sf(here("data/Prison_Boundaries/Prison_Boundaries.shp")) |>
   clean_names()
 
+prison_boundaries_sf$county <- str_to_title(prison_boundaries_sf$county)
+
 superfund_buffers_3m_sf <- st_set_crs(superfund_buffers_3m_sf, st_crs(county_sf))
 prison_boundaries_sf <- st_transform(prison_boundaries_sf, st_crs(county_sf))
 
 
 ### clean/prep data #########################################
 
+state_names <- state_sf |>
+  as.data.frame() |>
+  select(statefp, state = name)
+county_state_names_df <- county_sf |>
+  as.data.frame() |>
+  select(statefp, countyfp, county = name) |>
+  inner_join(state_names, by = c("statefp")) |>
+  mutate(countyfips = paste(statefp, countyfp, sep = "")) |>
+  select(county, state, countyfips)
+county_state_names_sf <- county_sf |>
+  select(statefp, countyfp, county = name) |>
+  inner_join(state_names, by = c("statefp")) |>
+  mutate(countyfips = paste(statefp, countyfp, sep = "")) |>
+  select(county, state, countyfips)
 
-
-
-
-### create outputs/maps/plots ###############################
-
+prison_boundaries_sf <- prison_boundaries_sf |>
+  select(-c("state", "county")) |>
+  inner_join(county_state_names_df, by = c("countyfips"))
 
 
 ### UI ##########################################
@@ -139,51 +135,19 @@ tags$footer(
 
 ##### UI: Maps Page #################################
 
-tabPanel("Interactive U.S. Map",
+tabPanel("Mapping by County",
          sidebarPanel(
            tags$div(
              selectInput(
-               inputId = "select_hazard",
-               label = "Select Hazard",
-               choices = c("Superfund Sites",
-                           "Heat Risk",
-                           "Flood Risk")
-             ),
-             p(strong("Superfund Sites"), "are designated by the EPA (include data link and further explanation here"),
-             p(strong("Heat Risk"), "data from (further explain and link)"),
-             p(strong("Flood Risk"), "data from (further explain and link)")
-           )
-         ), #end sidebarPanel
-
-         mainPanel(
-           tags$div(
-             h3("Mapping Carceral Facilities' Exposure to Environmental Hazards",
-                style = "font-weight: bold"),
-             p("On this page, we'll look at blah blah blah.")),
-           withSpinner(
-           tmapOutput("prison_map")),
-           tags$div(
-             p("Write some analysis of what we see on the map and what all this stuff means here."),
-             style = "padding: 10px 10px 0px 0px"
-           ),
-           tags$footer(
-             p("These maps were created using data provided by (list data sources here)"),
-             br(),
-             br()
-           )
-         ) #end mainPanel
-), #end Interactive Map
-
-tabPanel("Mapping by State",
-         sidebarPanel(
-           tags$div(
-             textInput(
-               inputId = "select_county",
-               label = "Select County",
-               value = "Type county name..."
+               inputId = "state",
+               label = "Select a state:",
+               choices = unique(prison_boundaries_sf$state)
              ), #end selectinput
-             hr(),
-             fluidRow(column(3, verbatimTextOutput("value")))
+             selectInput(
+               inputId = "county",
+               label = "Select a county:",
+               choices = NULL
+             )
            ), #end tags$div
            tags$div(
              selectInput(
@@ -196,6 +160,11 @@ tabPanel("Mapping by State",
              p(strong("Superfund Sites"), "are designated by the EPA (include data link and further explanation here"),
              p(strong("Heat Risk"), "data from (further explain and link)"),
              p(strong("Flood Risk"), "data from (further explain and link)")
+           ),
+           tags$footer(
+             p("These maps were created using data provided by (list data sources here)"),
+             br(),
+             br()
            )
          ), #end sidebarPanel
 
@@ -205,7 +174,7 @@ tabPanel("Mapping by State",
                 style = "font-weight: bold"),
              p("On this page, we'll look at blah blah blah.")),
            withSpinner(
-          plotOutput("county_plot")),
+          tmapOutput("county_plot")),
            tags$div(
              p("Write some analysis of what we see on the map and what all this stuff means here."),
              style = "padding: 10px 10px 0px 0px"
@@ -228,28 +197,51 @@ tabPanel("References")
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+
+  #Update the choices of county selectInput based on selected state
+  observeEvent(input$state, {
+    state_input <- input$state
+
+    choices <- prison_boundaries_sf |>
+      filter(state == state_input) |>
+      pull(county)
+
+    updateSelectInput(session, "county",
+                      choices = choices)
+  })
 
 
   tmap_mode("view")
-  output$test = renderTmap({
-    tm_shape(state_sf) +
-      tm_polygons()
+  output$county_plot <- renderTmap({
+    county_input <- input$county
+    state_input <- input$state
+    county_filtered <- county_state_names_sf |>
+      filter(state == state_input) |>
+      filter(county == county_input)
+    county_prisons <- prison_boundaries_sf |>
+      filter(state == state_input) |>
+      filter(county == county_input)
 
-  })
-
-  output$value <- renderPrint({ input$text })
-
-  county_reactive <- reactive({
-   county_sf %>%
-     filter(namelsad %in% input$pick_county)
-  }) #end state_reactive
-
-  output$county_plot <- renderPlot({
-    ggplot() +
-      geom_sf(data = county_reactive())
+    tm_shape(county_filtered) +
+      tm_fill(col = "lightgrey",
+              alpha = 0.3) +
+      tm_polygons() +
+      tm_shape(superfund_buffers_3m_sf) +
+      tm_fill(col = "red",
+              alpha = 0.3) +
+      tm_polygons() +
+      tm_shape(superfund_buffers_1m_sf) +
+      tm_fill(col = "darkred",
+              alpha = 0.3) +
+      tm_polygons() +
+      tm_shape(county_prisons) +
+      tm_dots("name",
+              palette = "Purples",
+              legend.show = FALSE)
     })
-  #end output$state_plot
+
+  #end output$county_plot
   #prison_boundaries_3_reactive <- reactive({
     #prison_boundaries_3_sf %>%
       #filter(state_1 %in% input$select_state)
